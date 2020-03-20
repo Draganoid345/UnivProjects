24030026eb063804541da0d315c6f766eb78651b
ConstantinDragancea <dragancea.constantin@gmail.com>
2020-03-21 00:55:24 +0200

    update main.cpp

--- SD/Project1/main.cpp
+++ SD/Project1/main.cpp
@@ -23,46 +23,7 @@ int main(){
         cout << "Incepe testul " << k << '\n';
         in >> n >> valmax >> negatives;
         arr = Utils::generateTest(n, valmax, negatives);
-        // cout << "Testul " << k << " :\n";
-        // cout << n << " numere mai mici sau egale in modul decat " << valmax << '\n';
-        // if (negatives) cout << "Sunt si numere negative\n\n";
-        // else cout << "Nu sunt numere negative\n\n";
-
-        // if (valmax > 1e8){
-        //     cout << "\tNumere prea mari pentru CountSort\n";
-        // }
-        // else{
-        //     cout << "\n\tCountSort:\n";
-        //     Utils::testTheSort(CountSort::Sort, arr);
-        // }
         
-
-        // if (n <= 1e4){
-        //     cout << "\n\tBubbleSort:\n";
-        //     Utils::testTheSort(BubbleSort::Sort, arr);
-        // }
-        // else cout << "\tPrea multe numere pentru BubbleSort\n";
-
-        // cout << "\n\tQuickSort:\n";
-        // Utils::testTheSort(QuickSort::Sort, arr);
-
-        // cout << "\n\tHeapSort:\n";
-        // Utils::testTheSort(HeapSort::Sort, arr);
-
-        // cout << "\n\tRadixSort in baza 16:\n";
-        // Utils::testTheSort(RadixSort::Sort16, arr);
-
-        // cout << "\n\tRadixSort in baza 256:\n";
-        // Utils::testTheSort(RadixSort::Sort256, arr);
-
-        // cout << "\n\tMergeSort:\n";
-        // Utils::testTheSort(MergeSort::Sort, arr);
-
-        // cout << "\n\tSTLSort:\n";
-        // Utils::testTheSort(STLSort::Sort, arr);
-
-        // cout << "\n\n";
-
         out << "Testul " << k << " :\n";
         out << n << " numere mai mici sau egale in modul decat " << valmax << '\n';
         if (negatives) out << "Sunt si numere negative\n\n";
